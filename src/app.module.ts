9141244bd1cef442bcf06bac2353ffb25ac2ead5
unknown <jjjk0605@naver.com>
2026-02-27 18:26:39 +0900

    feat: validator설정 및 typeorm설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { ScheduleModule } from '@nestjs/schedule';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    ScheduleModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      url: process.env.DATABASE_URL,
+      autoLoadEntities: true,
+      synchronize: true, // 개발환경에서만 true 배포시 false,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
