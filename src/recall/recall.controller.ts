8aba3e755e58e4aa463eda105f74a214422df136
unknown <jjjk0605@naver.com>
2026-02-28 17:07:07 +0900

    feat: 최근 리콜 제품을 반환하는 api구현

--- src/recall/recall.controller.ts
+++ src/recall/recall.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Query,
+} from '@nestjs/common';
 import { RecallService } from './recall.service';
 
 @Controller('recall')
 export class RecallController {
   constructor(private readonly recallService: RecallService) {}
+
+  @Get('recent')
+  getRecentRecall(
+    @Query('take', new DefaultValuePipe(5), ParseIntPipe) take: number,
+  ) {
+    return this.recallService.findRecentRecall(take);
+  }
 }
