8e38fcbd0debf1ba2364b7c36281fca73909d4be
SeongIn Ko <60644352+rhtjddls123@users.noreply.github.com>
2026-02-28 17:09:03 +0900

    [Feature/issue-006] 리콜 정보 반환 api구현 (#7)
    * fix: SafetyInfoModule을 import하지 않아 작동하지 않던 오류 수정
    
    * feat: 최근 리콜 제품을 반환하는 api구현

--- src/recall/recall.service.ts
+++ src/recall/recall.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RecallModel } from './entity/recall.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class RecallService {}
+export class RecallService {
+  constructor(
+    @InjectRepository(RecallModel)
+    private readonly recallRepository: Repository<RecallModel>,
+  ) {}
+
+  async findRecentRecall(take = 5) {
+    return this.recallRepository.find({
+      order: { recallSn: 'DESC' },
+      take,
+    });
+  }
+}
