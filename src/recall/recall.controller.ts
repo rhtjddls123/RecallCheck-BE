6facb593aba6fbb347c49906694cec788ba55d68
unknown <jjjk0605@naver.com>
2026-03-03 00:05:34 +0900

    feat: 필터링 기능을 가진 리콜제품 목록 페이지네이션 api구현

--- src/recall/recall.controller.ts
+++ src/recall/recall.controller.ts
@@ -6,15 +6,21 @@ import {
   Query,
 } from '@nestjs/common';
 import { RecallService } from './recall.service';
+import { PaginateRecallDto } from './dto/paginate-recall.dto';
 
 @Controller('recall')
 export class RecallController {
   constructor(private readonly recallService: RecallService) {}
 
   @Get('recent')
-  getRecentRecall(
+  async getRecentRecall(
     @Query('take', new DefaultValuePipe(5), ParseIntPipe) take: number,
   ) {
     return this.recallService.findRecentRecall(take);
+  }
+
+  @Get()
+  async getRecall(@Query() body: PaginateRecallDto) {
+    return this.recallService.findPaginateRecall(body);
   }
 }
