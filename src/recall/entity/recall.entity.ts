ffdce197948a3125212fc7ed760bc64fbaad89ab
unknown <jjjk0605@naver.com>
2026-03-03 00:02:03 +0900

    refactor: null이 가능한 값들에 대해 entity 타입 수정

--- src/recall/entity/recall.entity.ts
+++ src/recall/entity/recall.entity.ts
@@ -20,91 +20,91 @@ export class RecallModel {
   cntntsId: string; // 메뉴 ID
 
   @Index()
-  @Column({ type: 'text' })
+  @Column({ type: 'text', nullable: false })
   productNm: string; // 제품명
 
   @Column({ type: 'text', nullable: true })
-  makr?: string; // 제조사
+  makr: string | null; // 제조사
 
   @Index()
   @Column({ type: 'text', nullable: true })
-  bsnmNm?: string; // 사업자명
+  bsnmNm: string | null; // 사업자명
 
   @Column({ type: 'text', nullable: true })
-  modlNmInfo?: string; // 모델명 정보
+  modlNmInfo: string | null; // 모델명 정보
 
   @Column({ type: 'text', nullable: true })
-  stdBrcd?: string; // 표준 바코드
+  stdBrcd: string | null; // 표준 바코드
 
   @Column({ type: 'text', nullable: true })
-  mnfcturPd?: string; // 제조기간
+  mnfcturPd: string | null; // 제조기간
 
   @Column({ type: 'text', nullable: true })
-  mnfcturNoInfo?: string; // 제조번호 정보
+  mnfcturNoInfo: string | null; // 제조번호 정보
 
   @Column({ type: 'text', nullable: true })
-  distbTmlmtDe?: string; // 유통기한
+  distbTmlmtDe: string | null; // 유통기한
 
   @Column({ type: 'text', nullable: true })
-  prmisnNo?: string; // 허가번호
+  prmisnNo: string | null; // 허가번호
 
   @Column({ type: 'text', nullable: true })
-  aditfield13?: string; // 제품 상세내용
+  aditfield13: string | null; // 제품 상세내용
 
   @Column({ type: 'text', nullable: true })
-  shrtcomCn?: string; // 결함 내용
+  shrtcomCn: string | null; // 결함 내용
 
   @Column({ type: 'text', nullable: true })
-  recallSe?: string; // 리콜 구분
+  recallSe: string | null; // 리콜 구분
 
   @Column({ type: 'date', nullable: true })
-  recallPublictBgnde?: string | null; // 리콜 공표 시작일
+  recallPublictBgnde: string | null; // 리콜 공표 시작일
 
   @Column({ type: 'date', nullable: true })
-  recallPublictEndde?: string | null; // 리콜 공표 만료일
+  recallPublictEndde: string | null; // 리콜 공표 만료일
 
   @Column({ type: 'text', nullable: true })
-  injryCauseResult?: string; // 위해 원인
+  injryCauseResult: string | null; // 위해 원인
 
   @Column({ type: 'text', nullable: true })
-  hrmflGrad?: string; // 위해성 등급
+  hrmflGrad: string | null; // 위해성 등급
 
   @Column({ type: 'text', nullable: true })
-  acdntCn?: string; // 사고 내용
+  acdntCn: string | null; // 사고 내용
 
   @Column({ type: 'text', nullable: true })
-  cnsmrGhvrTips?: string; // 소비자 행동 요령
+  cnsmrGhvrTips: string | null; // 소비자 행동 요령
 
   @Column({ type: 'text', nullable: true })
-  trtmntAtpn?: string; // 취급 주의사항
+  trtmntAtpn: string | null; // 취급 주의사항
 
   @Column({ type: 'date', nullable: true })
-  recallBgnde?: string | null; // 리콜 시작일
+  recallBgnde: string | null; // 리콜 시작일
 
   @Column({ type: 'date', nullable: true })
-  recallEndde?: string | null; // 리콜 종료일
+  recallEndde: string | null; // 리콜 종료일
 
   @Column({ type: 'text', nullable: true })
-  recallProcssInfo?: string; // 리콜 절차
+  recallProcssInfo: string | null; // 리콜 절차
 
   @Column({ type: 'text', nullable: true })
-  recallEntrpsInfo?: string; // 문의처
+  recallEntrpsInfo: string | null; // 문의처
 
   @Column({ type: 'text', nullable: true })
-  infoOriginInstt?: string; // 정보 출처 기관
+  infoOriginInstt: string | null; // 정보 출처 기관
 
   @Column({ type: 'text', nullable: true })
-  infoOriginUrl?: string; // 정보 출처 URL
+  infoOriginUrl: string | null; // 정보 출처 URL
 
   @Column({ type: 'text', nullable: true })
-  infoCreatInstt?: string; // 정보 생성 기관
+  infoCreatInstt: string | null; // 정보 생성 기관
 
-  @Column({ type: 'text', nullable: true })
-  infoCreatUrl?: string[]; // 정보 생성 URL (||로 구분)
+  @Column({ type: 'simple-array', nullable: true })
+  infoCreatUrl: string[] | null; // 정보 생성 URL (||로 구분)
 
   // 쉼표 구분 문자열을 배열처럼 저장
   @Column({ type: 'simple-array', nullable: true })
-  recallImgUrls?: string[];
+  recallImgUrls: string[] | null;
 
   @CreateDateColumn()
   createdAt: Date;
