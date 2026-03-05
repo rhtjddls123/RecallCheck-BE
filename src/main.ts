a3c5c366a2ace33f09f4d120cdf3ccc607dc0468
unknown <jjjk0605@naver.com>
2026-03-05 15:35:17 +0900

    feat: cors경로 설정

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,15 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin:
+      process.env.NODE_ENV === 'production'
+        ? 'https://recall-check-fe.vercel.app'
+        : 'http://localhost:3001',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
