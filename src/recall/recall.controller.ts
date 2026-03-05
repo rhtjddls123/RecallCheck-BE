e59a5f0ebdcc010419b0cdf7a0e2bdf375fddec7
unknown <jjjk0605@naver.com>
2026-03-05 15:42:54 +0900

    feat: recall 컨트롤러 구성

--- src/recall/recall.controller.ts
+++ src/recall/recall.controller.ts
@@ -7,10 +7,18 @@ import {
 } from '@nestjs/common';
 import { RecallService } from './recall.service';
 import { PaginateRecallDto } from './dto/paginate-recall.dto';
+import type { RECALL_CATEGORY_KEY_TYPE } from 'src/consumer24/const/KEYS.const';
+import { InjectRepository } from '@nestjs/typeorm';
+import { RecallModel } from './entity/recall.entity';
+import { Repository } from 'typeorm';
 
 @Controller('recall')
 export class RecallController {
-  constructor(private readonly recallService: RecallService) {}
+  constructor(
+    private readonly recallService: RecallService,
+    @InjectRepository(RecallModel)
+    private readonly recallRepository: Repository<RecallModel>,
+  ) {}
 
   @Get('recent')
   async getRecentRecall(
@@ -23,4 +31,42 @@ export class RecallController {
   async getRecall(@Query() body: PaginateRecallDto) {
     return this.recallService.findPaginateRecall(body);
   }
+
+  @Get('correct-typo')
+  async getCorrectTypo(@Query('query') query: string) {
+    return this.recallService.correctQuery(query);
+  }
+
+  @Get('chatbot-search')
+  async chatbotSearch(
+    @Query('query') query: string,
+    @Query('categoryId') categoryId?: RECALL_CATEGORY_KEY_TYPE,
+  ) {
+    return this.recallService.chatbotSearch(query, categoryId);
+  }
+
+  @Get('embedding-search')
+  async embeddingSearch(@Query('query') query: string) {
+    return this.recallService.embeddingSearch(query);
+  }
+
+  // @Get('ping')
+  // async ping() {
+  //   return this.recallService.checkConnection();
+  // }
+
+  // @Post('create-index') // es 인덱스 생성
+  // async createIndex() {
+  //   return this.recallService.createIndex();
+  // }
+
+  // @Post('sync') // es db와 동기화
+  // async sync() {
+  //   return this.recallService.syncToElasticsearch();
+  // }
+
+  // @Post('embed-all') // 최초 1회만 실행(db에 임베딩값 저장)
+  // async embedAll() {
+  //   return this.recallService.embedAllProducts();
+  // }
 }
